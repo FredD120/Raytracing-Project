3f44007d7cd11e889dac944885adb39b39fd01d0
FredD120 <fw.daniels01@gmail.com>
2023-10-06 22:22:18 +0100

    Multiple spheres

--- MyFirstProject/Main.cpp
+++ MyFirstProject/Main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <cmath>
 #include <fstream>
+#include <list>
+#include <limits>
+
 
 class ThreeVector {
 public:
@@ -8,9 +11,7 @@ public:
 	double y;
 	double z;
 
-	ThreeVector() : x(1), y(1), z(1) {
-		std::cout << "Default vector created" << std::endl;
-	}
+	ThreeVector() : x(1), y(1), z(1) {}
 
 	ThreeVector(double entry1, double entry2, double entry3) :
 		x(entry1), y(entry2), z(entry3) {}
@@ -65,9 +66,7 @@ public:
 
 	Ray(ThreeVector& o, ThreeVector& d) : origin(o), direction(d) {}
 
-	Ray() : origin(ThreeVector()), direction(ThreeVector()) {
-		std::cout << "Default ray created" << std::endl;
-	}
+	Ray() : origin(ThreeVector()), direction(ThreeVector()) {}
 };
 
 class Sphere {
@@ -78,6 +77,8 @@ public:
 
 	Sphere(ThreeVector& cen, double rad, ThreeVector col) : centre(cen), radius(rad), colour(col){}
 
+	Sphere() : centre(ThreeVector()), radius(1), colour(ThreeVector()) {}
+
 	double intersect(Ray current_ray) {
 		ThreeVector origin_to_center = current_ray.origin - centre;
 		double a = current_ray.direction.dot(current_ray.direction);
@@ -85,60 +86,30 @@ public:
 		double c = origin_to_center.dot(origin_to_center) - radius * radius;
 		double discriminant = b * b - 4 * a * c;
 		if (discriminant > 0) {
-			double distance1 = (-b - std::sqrt(discriminant)) / (2.0 * a); //another solution exists, but it's at the back of the sphere
-			if (0 < distance1) {
+			double distance1 = (-b - std::sqrt(discriminant)) / (2.0 * a);
+				if (distance1 > 0) {
+					return distance1;
+				}
+			/*
+			double distance1 = (-b - std::sqrt(discriminant)) / (2.0 * a); //alternate way to calculate using both solutions
+			double distance2 = (-b + std::sqrt(discriminant)) / (2.0 * a);
+			if (distance1 > 0 && distance2 > distance1) {
+				return distance2;
+			}
+			else if (distance1 > 0) {
 				return distance1;
 			}
+			*/
 		}
-		else {
-			return -1;
-		}
+		return std::numeric_limits<double>::max();
 	}
 
 	ThreeVector normal(ThreeVector position) {
-		return (centre - position).normalise();
+		return (position - centre).normalise();
 	}
 };
 
-
-class IntArray {
-public:
-	static const int xsize = 10;
-	static const int ysize = 10;
-	int arr[xsize][ysize];
-	int inital_value = 0;
-
-	IntArray(int inval) {
-		int initial_value = inval;
-		for (int i = 0; i < xsize; i++) {
-			for (int j = 0; j < ysize; j++) {
-				arr[i][j] = initial_value;
-			}
-		}
-	}
-
-	void print_array() {
-		for (int i = 0; i < xsize; i++) {
-			for (int j = 0; j < ysize; j++) {
-				std::cout << arr[i][j] << " ";
-			}
-		}
-	}
-
-	void save_array(std::string FileName) {
-		std::ofstream NewFile;
-		NewFile.open(FileName);
-		for (int i = 0; i < xsize; i++) {
-			for (int j = 0; j < ysize; j++) {
-				NewFile << arr[i][j] << " ";
-			}
-			NewFile << "\n" ;
-		}
-		NewFile.close();
-	}
-};
-
-void render(Sphere object){
+void render(std::list<Sphere> objects){
 	std::ofstream ImageFileR("C:/Users/fwdan/C++ Raytracing/ImageDataR.txt");
 	std::ofstream ImageFileG("C:/Users/fwdan/C++ Raytracing/ImageDataG.txt");
 	std::ofstream ImageFileB("C:/Users/fwdan/C++ Raytracing/ImageDataB.txt");
@@ -148,11 +119,11 @@ void render(Sphere object){
 
 	double lightsource_brightness = 0.8;
 	double ambient_brightness = 1-lightsource_brightness;
-	ThreeVector lightsource_position(1,5,-1);
+	ThreeVector lightsource_position(1.5,1,-2);
 
-	ThreeVector camera_position(0, 0, 0);
+	ThreeVector camera_position(0, 0, -5);
 	ThreeVector camera_up(0, 1, 0);
-	ThreeVector camera_direction(0, 0, 1);
+	ThreeVector camera_direction(0, -0.2, 1);
 	ThreeVector camera_right = camera_direction.cross(camera_up);
 
 	for (int y = 0; y < height; y++) {
@@ -161,17 +132,27 @@ void render(Sphere object){
 			double v = -(y - height / 2.0) / static_cast<double>(height);
 			ThreeVector ray_direction = camera_direction + camera_right.scalar_product(u) + camera_up.scalar_product(v);
 			Ray ray1(camera_position, ray_direction);
-			double min_distance = object.intersect(ray1);
-			if (min_distance > 0) {
+
+			double min_distance = std::numeric_limits<double>::max();
+			Sphere* current_object_pointer = nullptr;
+
+			for (Sphere& object : objects) {
+				double current_distance = object.intersect(ray1);
+				if (current_distance < min_distance) {
+					min_distance = current_distance;
+					current_object_pointer = &object;
+				}
+			}			
+			if (current_object_pointer != nullptr) {
 				ThreeVector intersect_pos = camera_position + ray_direction.scalar_product(min_distance);
-				ThreeVector light_to_sphere = (lightsource_position - intersect_pos).normalise().invert();
-				double lightsource_reflection = lightsource_brightness * object.normal(intersect_pos).dot(light_to_sphere);
-				ThreeVector nearest_colour = object.colour;
+				ThreeVector light_to_sphere = (lightsource_position - intersect_pos).normalise();
+				double lightsource_reflection = lightsource_brightness * current_object_pointer->normal(intersect_pos).dot(light_to_sphere);
+				ThreeVector nearest_colour = ThreeVector();
 				if (lightsource_reflection>0){
-					nearest_colour = object.colour.scalar_product(ambient_brightness + lightsource_reflection);
+					nearest_colour = current_object_pointer->colour.scalar_product(ambient_brightness + lightsource_reflection);
 				}
 				else {
-					nearest_colour = object.colour.scalar_product(ambient_brightness);
+					nearest_colour = current_object_pointer->colour.scalar_product(ambient_brightness);
 				}
 				ImageFileR << static_cast<int>(nearest_colour.x) << " ";
 				ImageFileG << static_cast<int>(nearest_colour.y) << " ";
@@ -194,11 +175,28 @@ void render(Sphere object){
 
 int main() {
 	
-	ThreeVector Centre1(0, 0, 3);
+	ThreeVector Centre1(1, 0.5, 6);
 	double radius1 = 1;
-	ThreeVector colour1(0, 200, 200);
+	ThreeVector colour1(200, 0, 200);
+
+	ThreeVector Centre2(-1, -0.5, 4);
+	double radius2 = 1.4;
+	ThreeVector colour2(0, 150, 250);
+
+	ThreeVector Centre3(0, -100, 3);
+	double radius3 = 95;
+	ThreeVector colour3(50, 250, 50);
+
+	ThreeVector light(2, -1, 3);
+	double light_radius = 0.5;
+	ThreeVector light_colour(250, 250, 250);
+
 	Sphere object1(Centre1, radius1, colour1);
-	render(object1);
+	Sphere object2(Centre2, radius2, colour2);
+	Sphere object3(Centre3, radius3, colour3);
+	Sphere lightsource(light, light_radius, light_colour);
+	std::list<Sphere> scene = {object1,object2,object3,lightsource};
+	render(scene);
 	
 	return 0;
 }
